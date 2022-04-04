dc9f97c7c2c4c34004c5af63bfa0533943e7c3dd
waiga <hyang9387@gmail.com>
2022-04-04 20:13:59 +0900

    Initial Commit

---
+++ ApiQuestionDemo/ApiQuestionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiQuestionDemoApp.swift
+//  ApiQuestionDemo
+//
+//  Created by 김미향 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct ApiQuestionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
